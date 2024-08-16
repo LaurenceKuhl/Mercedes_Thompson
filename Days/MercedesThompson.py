8b9b76ea0eae2ab6ebad154d01e29fb4e7fae5df
laurencekuhl <laurence.kuhlburger@gmail.com>
2024-08-16 16:41:42 +0200

    Add

--- Days/MercedesThompson.py
+++ Days/MercedesThompson.py
@@ -9,22 +9,25 @@ Description: day 1 of my new coding habit
 import datetime
 from abc import ABC
 from typing import Any
+
 import click
 
 
 class Creature:
-    """ Creates a Creature which could be a human, werewolf or shapeshifter """
+    """Creates a Creature which could be a human, werewolf or shapeshifter"""
+
     def __init__(self, name: str, gender: str, birthday: datetime.date) -> None:
         self.name = name
         self.gender = gender
         self.birthday = birthday
 
     def __repr__(self):
-        return f'Creature(\'{self.name}\', {self.gender},  {self.birthday})'
+        return f"Creature('{self.name}', {self.gender},  {self.birthday})"
 
     def speaks(self, lines: str) -> None:
         return print(f"{self.name} says '{lines}'")
-    
+
+
 class Shapeshifter(Creature):
     def __init__(self, name, gender, birthday, animal) -> None:
         super().__init__(name, gender, birthday)
@@ -32,25 +35,28 @@ class Shapeshifter(Creature):
         self.human_shape = True
 
     def __repr__(self):
-        return f'Shapeshifter(\'{self.name}\', {self.gender},  {self.animal})'
-    
+        return f"Shapeshifter('{self.name}', {self.gender},  {self.animal})"
+
     def shapeshift(self) -> None:
-        if(self.human_shape): 
+        if self.human_shape:
             self.human_shape = False
             print(f"{self.name} swiftly turns into a {self.animal}")
         else:
             self.human_shape = True
             print(f"{self.name} swiftly turns back into a human!")
 
+
 class Werewolf(Creature):
-    def __init__(self, name, gender, birthday: datetime, fur_color, dominance: bool, pack):
+    def __init__(
+        self, name, gender, birthday: datetime, fur_color, dominance: bool, pack
+    ):
         super().__init__(name, gender, birthday)
         self.fur_color = fur_color
         self.dominance = dominance
         self.pack = pack
 
     def __repr__(self):
-        return f'Werewolf(\'{self.name}\', {self.gender},  {self.fur_color})'
+        return f"Werewolf('{self.name}', {self.gender},  {self.fur_color})"
 
     def rare(self) -> bool:
         fur = {
@@ -65,17 +71,25 @@ class Werewolf(Creature):
         }
         return fur[self.fur_color]
 
-class City: 
-    def __init__(self, name: str, gender: str, birthday: datetime, fur_color, dominance: bool, pack):
+
+class City:
+    def __init__(
+        self,
+        name: str,
+        gender: str,
+        birthday: datetime,
+        fur_color,
+        dominance: bool,
+        pack,
+    ):
         pass
-        
 
 
 # @click.command()
 # @click.option('-p', '--p', help='Specify which character to create')
 
 if __name__ == "__main__":
-    mercy = Creature("Mercy", "F", datetime.datetime(1989, 6, 1)) 
+    mercy = Creature("Mercy", "F", datetime.datetime(1989, 6, 1))
     mercy.speaks("Hey everyone")
     print(repr(mercy))
     adam = Werewolf(
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Welcome to my 100 day coding challenge !
 
-As I haven't programmed in object oriented in a while, I figured I'd  challenge myself to a 100 days of minimum 20min of coding per day in Python!
+As I haven't programmed in object oriented in a while, I figured I'd challenge myself to a 100 days of minimum 20min of coding per day in Python!
 
 What is object-oriented programming (OOP)? [Real Python](https://realpython.com/python3-object-oriented-programming/) describes it better, but it is a way of defining code and functions through specific objects. In my case, I want to recreate a fantasy world I grew up with, the Mercedes Thompson series by Patricia Briggs.
 
@@ -13,7 +13,7 @@ Mercedes Thompson is a mechanic who lives in the Tri State area. She also happen
 - classes
 - classes inheritances
 - pre-commit
-- the __init__ function
+- the **init** function
 - dictionaries
 
 ## New notions
