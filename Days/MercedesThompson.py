c51630304ef4ee44f91139a7cc405ed4c96f7f63
laurencekuhl <laurence.kuhlburger@gmail.com>
2024-08-16 17:06:27 +0200

    Added docstrings

--- Days/MercedesThompson.py
+++ Days/MercedesThompson.py
@@ -7,37 +7,36 @@ Description: day 1 of my new coding habit
 """
 
 import datetime
-from abc import ABC
-from typing import Any
-
-import click
 
 
 class Creature:
     """Creates a Creature which could be a human, werewolf or shapeshifter"""
-
     def __init__(self, name: str, gender: str, birthday: datetime.date) -> None:
         self.name = name
         self.gender = gender
         self.birthday = birthday
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """function printing the creature's name gender and birthday returns string"""
         return f"Creature('{self.name}', {self.gender},  {self.birthday})"
 
     def speaks(self, lines: str) -> None:
+        """function that prints the name of the character and the lines they say """
         return print(f"{self.name} says '{lines}'")
 
-
 class Shapeshifter(Creature):
+    """Creates a Shapeshifter which turns into a specific animal"""
     def __init__(self, name, gender, birthday, animal) -> None:
         super().__init__(name, gender, birthday)
         self.animal = animal
         self.human_shape = True
 
     def __repr__(self):
+        """returns the attributes of the shapeshifter """
         return f"Shapeshifter('{self.name}', {self.gender},  {self.animal})"
 
     def shapeshift(self) -> None:
+        """turns the shapeshifter back into a human or into its animal """
         if self.human_shape:
             self.human_shape = False
             print(f"{self.name} swiftly turns into a {self.animal}")
@@ -47,7 +46,8 @@ class Shapeshifter(Creature):
 
 
 class Werewolf(Creature):
-    def __init__(
+    """Creates a Werewolf which is either dominant or submissive and has a fur color"""
+    def __init__( #pylint: disable=too-many-arguments
         self, name, gender, birthday: datetime, fur_color, dominance: bool, pack
     ):
         super().__init__(name, gender, birthday)
@@ -56,9 +56,18 @@ class Werewolf(Creature):
         self.pack = pack
 
     def __repr__(self):
-        return f"Werewolf('{self.name}', {self.gender},  {self.fur_color})"
+        return (
+            f"Werewolf(\n"
+            f"    '{self.name}',\n"
+            f"    {self.gender},\n"
+            f"    {self.fur_color},\n"
+            f"    dominance: {self.dominance}\n"
+            f")"
+        )
+
 
     def rare(self) -> bool:
+        """Returns true if the fur color is rare and false if not"""
         fur = {
             "Pastel": True,
             "Glow": False,
@@ -72,8 +81,9 @@ class Werewolf(Creature):
         return fur[self.fur_color]
 
 
-class City:
-    def __init__(
+class City: #pylint: disable=too-few-public-methods
+    """STILL under construction"""
+    def __init__( #pylint: disable=too-many-arguments
         self,
         name: str,
         gender: str,
