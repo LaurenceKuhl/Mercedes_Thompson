e663005bf41d133d194bde63891ef609d26487aa
laurencekuhl <laurence.kuhlburger@gmail.com>
2024-08-18 10:16:24 +0200

    Fix linting and shape up the code a bit more

--- Days/mercedes_universe.py
+++ Days/mercedes_universe.py
@@ -5,16 +5,19 @@ Email: laurence.kuhlburger@gmail.com
 Github: https://github.com/laurencekuhl
 Description: day 1 of my new coding habit
 """
-
+from __future__ import annotations
 import datetime
+from typing import List, Optional
+
 
 class Creature:
     """Creates a Creature which could be a human, werewolf or shapeshifter"""
 
-    def __init__(self, name: str, gender: str, birthday: datetime.date) -> None:
+    def __init__(self, name: str, gender: str, birthday: datetime.date, city: City = None) -> None:
         self.name = name
         self.gender = gender
         self.birthday = birthday
+        self.city = city if city is not None else None
 
     def __repr__(self) -> str:
         """function printing the creature's name gender and birthday returns string"""
@@ -87,22 +90,31 @@ class Werewolf(Creature):
             "Mahogany": False,
         }
         return fur[self.fur_color]
+class City:
+    """Defines a city of inhabitants, all of whom are Creatures"""
+    def __init__(self, name: str):
+        self.name = name
+        self.creatures: List[Creature] = []  # Use forward reference with List[Creature]
 
+    def add_inhabitant(self, creature: Creature):
+        """Adds an inhabitant to the city"""
+        self.creatures.append(creature)
+        creature.city = self  # Set the city for the creature
 
-class City:  # pylint: disable=too-few-public-methods
-    """STILL under construction"""
+    def remove_inhabitant(self, creature: Creature):
+        """Removes inhabitant from the city in case they move or pass away"""
+        self.creatures.remove(creature)
+        creature.city = None  # Set the city for the creature
 
-    def __init__(  # pylint: disable=too-many-arguments
-        self,
-        name: str,
-        gender: str,
-        birthday: datetime,
-        fur_color,
-        dominance: bool,
-        pack,
-    ):
-        pass
+    def survey_population(self) -> List[str]:
+        """Shows a list of different inhabitants of the city"""
+        return [f"{creature.name} ({creature.species})" for creature in self.creatures]
 
+    @property
+    def population_size(self) -> int:
+        """returns the size of the population"""
+        print(len(self.creatures))
+        return len(self.creatures)
 
 if __name__ == "__main__":
     mercy = Creature("Mercy", "F", datetime.datetime(1989, 6, 1))
